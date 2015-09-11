5d15aa3800bd4755057a40f97714e99e90b8c3d0
p-gibbens <phg49389@bethel.edu>
2015-09-11 12:07:25 -0500

    Clearing unnecessary and private files from Git

--- classifieds/run.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'phg49389'
-
-from flask import Flask
-from classifieds.views import ClassifiedsView
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    ClassifiedsView.register(app)
-    app.run(debug=True)
