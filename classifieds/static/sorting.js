14d09aaedbfab0261b821c8c68057b917cd765d5
tih99924 <tih99924@bethel.edu>
2016-04-18 13:51:35 -0500

    adding js for sorting divs

---
+++ classifieds/static/sorting.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tih99924 on 4/15/16.
+ */
+
+document.getElementById("titleButton").addEventListener("click", sorting);
+
+function sorting() {
+
+    document.body.style.backgroundColor = "red";
+
+    //jQuery(".table > .singlePost > .row").sort(function (a, b) {
+    //    var upA = jQuery('#title', a).text().toUpperCase();
+    //    var upB = jQuery('#title', b).text().toUpperCase();
+    //    return upA > upB;
+    //}).appendTo('.table');
+
+};
