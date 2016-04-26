1078594b4432b23159d2284802ce53577a775d90
tih99924 <tih99924@bethel.edu>
2016-04-26 14:59:21 -0500

    Start on conversion to jQuery to get all browsers to work

--- classifieds/static/sorting.js
+++ classifieds/static/sorting.js
@@ -1,17 +1,17 @@
-/**
- * Created by tih99924 on 4/15/16.
- */
-
-document.getElementById("titleButton").addEventListener("click", function(){sortPosts(".title > a")});
-            document.getElementById("descriptionButton").addEventListener("click", function(){sortPosts(".description > article")});
-            document.getElementById("priceButton").addEventListener("click", function(){sortPosts(".price")});
-            document.getElementById("dateButton").addEventListener("click", function(){sortPosts(".date")});
-            document.getElementById("postedByButton").addEventListener("click", function(){sortPosts(".postedBy > a")});
-
-            function sortPosts(sortQuery) {
-                jQuery(".table > .singlePost").sort(function (a, b) {
-                    var upA = jQuery('> .row > ' + sortQuery, a).text().toUpperCase();
-                    var upB = jQuery('> .row > ' + sortQuery, b).text().toUpperCase();
-                    return upA > upB;
-                }).appendTo('.table');
-            };
+///**
+// * Created by tih99924 on 4/15/16.
+// */
+//
+//document.getElementById("titleButton").addEventListener("click", function(){sortPosts(".title > a")});
+//            document.getElementById("descriptionButton").addEventListener("click", function(){sortPosts(".description > article")});
+//            document.getElementById("priceButton").addEventListener("click", function(){sortPosts(".price")});
+//            document.getElementById("dateButton").addEventListener("click", function(){sortPosts(".date")});
+//            document.getElementById("postedByButton").addEventListener("click", function(){sortPosts(".postedBy > a")});
+//
+//            function sortPosts(sortQuery) {
+//                jQuery(".table > .singlePost").sort(function (a, b) {
+//                    var upA = jQuery('> .row > ' + sortQuery, a).text().toUpperCase();
+//                    var upB = jQuery('> .row > ' + sortQuery, b).text().toUpperCase();
+//                    return upA > upB;
+//                }).appendTo('.table');
+//            };
